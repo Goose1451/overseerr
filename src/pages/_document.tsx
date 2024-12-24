4b2ba248381e8d40c5a808e42c80e8cff9ae0719
Goose1451 <wgodard@gmail.com>
2024-12-23 16:15:59 -0800

    Update _document.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,19 +1,35 @@
+import newrelic from 'newrelic';
 import type { DocumentContext, DocumentInitialProps } from 'next/document';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 
-class MyDocument extends Document {
+type NewRelicProps = {
+  browserTimingHeader: string;
+};
+
+class MyDocument extends Document<NewRelicProps> {
   static async getInitialProps(
     ctx: DocumentContext
-  ): Promise<DocumentInitialProps> {
+  ): Promise<DocumentInitialProps & NewRelicProps> {
     const initialProps = await Document.getInitialProps(ctx);
 
-    return initialProps;
+    const browserTimingHeader = newrelic.getBrowserTimingHeader({
+      hasToRemoveScriptWrapper: true,
+    });
+
+    return {
+      ...initialProps,
+      browserTimingHeader,
+    };
   }
 
   render(): JSX.Element {
     return (
       <Html>
         <Head>
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{ __html: this.props.browserTimingHeader }}
+          />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             rel="stylesheet"
